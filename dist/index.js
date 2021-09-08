74fe41963f2181d25392985290c95bcd1da42755
Chris <chris.zirkel@gmail.com>
2021-09-08 13:30:01 +0200

    v1

--- dist/index.js
+++ dist/index.js
@@ -6341,27 +6341,27 @@ try {
   const instanceId = core.getInput('instance-id');
 	console.log(`Start EC2 instance ${instanceId}!`);
 
-	const startInstance = child_process.spawnSync('aws ec2 start-instances', ['--instance-ids', instanceId]);
-	console.log('status: ' + startInstance.status);
+	const startInstance = child_process.execSync('aws ec2 start-instances', ['--instance-ids', instanceId]).toString();
+	// console.log('status: ' + startInstance.status);
 	// console.log('stdout: ' + startInstance.stdout.toString('utf8'));
 	// console.log('stderr: ' + startInstance.stderr.toString('utf8'));
 	
-	const out = JSON.stringify(startInstance, undefined, 2);
-	console.log('output', out);
+	// const out = JSON.stringify(startInstance, undefined, 2);
+	console.log('output', startInstance);
 
-	const output = JSON.parse(startInstance.stdout.toString('utf8'));
-	const error = JSON.parse(startInstance.stderr.toString('utf8'));
+	const output = JSON.parse(startInstance);
+	// const error = JSON.parse(startInstance.stderr.toString('utf8'));
 
-	for (let state of output.StartingInstances) {
-		const previous = state.PreviousState.Name;
-		const current = state.CurrentState.Name;
+	// for (let state of output.StartingInstances) {
+	// 	const previous = state.PreviousState.Name;
+	// 	const current = state.CurrentState.Name;
 
-		console.log(`Previous state ${previous}!`);
-		core.setOutput("previous-state", previous);
+	// 	console.log(`Previous state ${previous}!`);
+	// 	core.setOutput("previous-state", previous);
 
-		console.log(`Current state ${current}!`);
-		core.setOutput("current-state", current);
-	}
+	// 	console.log(`Current state ${current}!`);
+	// 	core.setOutput("current-state", current);
+	// }
 
 	const json = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
